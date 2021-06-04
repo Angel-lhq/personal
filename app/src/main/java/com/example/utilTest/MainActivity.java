f6c85e3107b73ec5746bfd11f466dea438ed84dc
luo_hongqiang <luo_hongqiang@topsec.com.cn>
2021-06-04 17:58:14 +0800

    自定义加载弹窗

--- app/src/main/java/com/example/utilTest/MainActivity.java
+++ app/src/main/java/com/example/utilTest/MainActivity.java
@@ -1,26 +1,54 @@
 package com.example.utilTest;
 
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.example.kotlinlibrary.Kotlin;
+import com.example.utilTest.views.LoadingDialog;
 
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar mToolbar;
+    private LoadingDialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initView();
+//        initView();
+        showLoadingDialog();
     }
 
+    private void showLoadingDialog() {
+        //加载弹窗
+        LoadingDialog.Builder loadBuilder = new LoadingDialog.Builder(this)
+                .setMessage("加载中...")
+                .setCancelable(true)//返回键是否可点击
+                .setCancelOutside(false);//窗体外是否可点击
+        dialog = loadBuilder.create();
+        dialog.show();//显示弹窗
+        handler.postDelayed(runnable,1000);
+    }
+
+    //简单倒计时 10秒后加载弹窗消失
+    private int time = 10;
+    private final Handler handler = new Handler();
+    private final Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            if (time == 0){
+                dialog.dismiss();
+                return;
+            }
+            time --;
+            handler.postDelayed(runnable,1000);
+        }
+    };
+
     private void initView() {
-        mToolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(mToolbar);
         Kotlin kotlin = new Kotlin();
         kotlin.add(1);
         kotlin.add(2);
---
+++ app/src/main/java/com/example/utilTest/views/LoadingDialog.java
@@ -0,0 +1,107 @@
+package com.example.utilTest.views;
+
+import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.utilTest.R;
+import com.luo.loading.AVLoadingIndicatorView;
+
+/**
+ * 自定义加载弹窗 可根据需求修改弹窗中的的显示内容
+ * Custom loading window can modify the display content of the pop-up window according to the requirements
+ */
+public class LoadingDialog extends Dialog {
+
+
+    public LoadingDialog(Context context) {
+        super(context);
+    }
+
+    public LoadingDialog(Context context, int themeResId) {
+        super(context, themeResId);
+    }
+
+    public static class Builder {
+
+        private Context context;
+        private String message;
+        private boolean isShowMessage = true;
+        private boolean isCancelable = false;
+        private boolean isCancelOutside = false;
+
+
+        public Builder(Context context) {
+            this.context = context;
+        }
+
+        /**
+         * 设置提示信息
+         *
+         * @param message
+         * @return
+         */
+
+        public Builder setMessage(String message) {
+            this.message = message;
+            return this;
+        }
+
+        /**
+         * 设置是否显示提示信息
+         *
+         * @param isShowMessage
+         * @return
+         */
+        public Builder setShowMessage(boolean isShowMessage) {
+            this.isShowMessage = isShowMessage;
+            return this;
+        }
+
+        /**
+         * 设置是否可以按返回键取消
+         *
+         * @param isCancelable
+         * @return
+         */
+
+        public Builder setCancelable(boolean isCancelable) {
+            this.isCancelable = isCancelable;
+            return this;
+        }
+
+        /**
+         * 设置是否可以取消
+         *
+         * @param isCancelOutside
+         * @return
+         */
+        public Builder setCancelOutside(boolean isCancelOutside) {
+            this.isCancelOutside = isCancelOutside;
+            return this;
+        }
+
+        @SuppressLint("WrongConstant")
+        public LoadingDialog create() {
+
+            LayoutInflater inflater = LayoutInflater.from(context);
+            View view = inflater.inflate(R.layout.dialog_loading, null);
+            LoadingDialog loadingDailog = new LoadingDialog(context, R.style.CustomDialog);
+            AVLoadingIndicatorView loading = view.findViewById(R.id.avloadingIndicatorView_BallSpinFadeLoader);
+            TextView msgText = (TextView) view.findViewById(R.id.tipTextView);
+            if (isShowMessage) {
+                msgText.setText(message);
+            } else {
+                msgText.setVisibility(View.GONE);
+            }
+            loadingDailog.setContentView(view);
+            loadingDailog.setCancelable(isCancelable);
+            loadingDailog.setCanceledOnTouchOutside(isCancelOutside);
+            return loadingDailog;
+
+        }
+    }
+}
